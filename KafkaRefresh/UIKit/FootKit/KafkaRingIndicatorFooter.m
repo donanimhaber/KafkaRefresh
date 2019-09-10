044eb37623ec0ab29ffe36cb7c692ce20a721808
hsynvrl <huseyinvural@live.com>
2019-09-10 17:21:27 +0300

    DH indents optimization.

--- KafkaRefresh/UIKit/FootKit/KafkaRingIndicatorFooter.m
+++ KafkaRefresh/UIKit/FootKit/KafkaRingIndicatorFooter.m
@@ -8,88 +8,91 @@
  * This source code is licensed under the MIT license.
  */
 
-#import "KafkaRingIndicatorFooter.h" 
+#import "KafkaRingIndicatorFooter.h"
 #import "KafkaCategories.h"
 
 @implementation KafkaRingIndicatorFooter
 
 - (void)setupProperties{
-	[super setupProperties];
-	[self.layer addSublayer:self.arcLayer];
-	[self addSubview:self.indicator];
+  [super setupProperties];
+  [self.layer addSublayer:self.arcLayer];
+  [self addSubview:self.indicator];
 }
 
 - (void)layoutSubviews{
-	[super layoutSubviews];
-	self.arcLayer.frame = CGRectMake(0, 0, self.kr_width, self.kr_height);
-	self.indicator.center = CGPointMake(self.kr_width/2., self.kr_height/2.);
+  [super layoutSubviews];
+  self.arcLayer.frame = CGRectMake(0, 0, self.kr_width, self.kr_height);
+  self.indicator.center = CGPointMake(self.kr_width/2., self.kr_height/2.);
 }
 
 - (void)setThemeColor:(UIColor *)fillColor{
-	if (super.themeColor == fillColor) {
-		return;
-	}
-	[super setThemeColor:fillColor];
-	self.arcLayer.ringFillColor = fillColor; 
+  if (super.themeColor == fillColor) {
+    return;
+  }
+  [super setThemeColor:fillColor];
+  self.arcLayer.ringFillColor = fillColor;
 }
 
 - (void)setAnimatedBackgroundColor:(UIColor *)animatedBackgroundColor{
-	if (super.animatedBackgroundColor == animatedBackgroundColor) {
-		return;
-	}
-	[super setAnimatedBackgroundColor:animatedBackgroundColor];
-	self.arcLayer.ringBackgroundColor = animatedBackgroundColor;
+  if (super.animatedBackgroundColor == animatedBackgroundColor) {
+    return;
+  }
+  [super setAnimatedBackgroundColor:animatedBackgroundColor];
+  self.arcLayer.ringBackgroundColor = animatedBackgroundColor;
 }
 
 - (void)kafkaDidScrollWithProgress:(CGFloat)progress max:(const CGFloat)max{
 #define kOffset 0.3
-	if (progress >= kOffset) {
-		progress = (progress-kOffset)/(max - kOffset);
-		[self.arcLayer setProgress:progress];
-	}
+  if (progress >= kOffset) {
+    progress = (progress-kOffset)/(max - kOffset);
+    [self.arcLayer setProgress:progress];
+  }
 }
 
 - (void)kafkaRefreshStateDidChange:(KafkaRefreshState)state{
-	[super kafkaRefreshStateDidChange:state];
-	switch (state) {
-		case KafkaRefreshStateNone:{
-			break;
-		}
-		case KafkaRefreshStateScrolling:{
-			break;
-		}
-		case KafkaRefreshStateReady:{
-			self.arcLayer.opacity = 1.;
-			break;
-		}
-		case KafkaRefreshStateRefreshing:{
-			[self.arcLayer startAnimating];
-			self.arcLayer.hidden = YES;
-			[self.indicator startAnimating];
-			break;
-		}
-		case KafkaRefreshStateWillEndRefresh:{
-			[self.indicator stopAnimating];
-			self.arcLayer.hidden = NO;
-			[self.arcLayer stopAnimating];
-			break;
-		}
-	}
+  [super kafkaRefreshStateDidChange:state];
+  switch (state) {
+    case KafkaRefreshStateNone:{
+      self.arcLayer.hidden = YES;
+      [self.arcLayer setProgress:0.0];
+      break;
+    }
+    case KafkaRefreshStateScrolling:{
+      self.arcLayer.hidden = NO;
+      break;
+    }
+    case KafkaRefreshStateReady:{
+      self.arcLayer.opacity = 1.;
+      break;
+    }
+    case KafkaRefreshStateRefreshing:{
+      [self.arcLayer startAnimating];
+      self.arcLayer.hidden = YES;
+      [self.indicator startAnimating];
+      break;
+    }
+    case KafkaRefreshStateWillEndRefresh:{
+      [self.indicator stopAnimating];
+      self.arcLayer.hidden = NO;
+      [self.arcLayer stopAnimating];
+      break;
+    }
+  }
 }
 
 - (KafkaArcLayer *)arcLayer{
-	if (!_arcLayer) {
-		_arcLayer = [KafkaArcLayer layer];
-	}
-	return _arcLayer;
+  if (!_arcLayer) {
+    _arcLayer = [KafkaArcLayer layer];
+  }
+  return _arcLayer;
 }
 
 - (UIActivityIndicatorView *)indicator{
-	if (!_indicator) {
-		_indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
-		_indicator.hidesWhenStopped = YES;
-	}
-	return _indicator;
+  if (!_indicator) {
+    _indicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    _indicator.hidesWhenStopped = YES;
+  }
+  return _indicator;
 }
 
 @end
