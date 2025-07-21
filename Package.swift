69153a712b26cd90593599ceab40dc5b59668fce
Hasan Aygünoğlu <hasanaygunoglu@gmail.com>
2025-07-21 21:50:42 +0300

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+import PackageDescription
+
+let package = Package(
+    name: "KafkaRefresh", // The name of your Swift Package
+    platforms: [
+        .iOS(.v15.0) // Or the minimum iOS version KafkaRefresh supports
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "KafkaRefresh",
+            targets: ["KafkaRefresh"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "KafkaRefresh",
+            path: "KafkaRefresh", // Path to the source files of KafkaRefresh within the repository
+            exclude: ["Demo", "Assets", "README.md", "LICENSE", "KafkaRefresh.podspec"] // Exclude unnecessary files/folders
+        )
+    ]
+)
